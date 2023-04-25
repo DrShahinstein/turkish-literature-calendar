793350f0179e73401948df3c666ac41e3156e156
DrShahinstein <1taylanozturk@protonmail.com>
2023-04-25 23:27:31 +0300

    Export extra constants in utilities

--- src/calendar-utilities.js
+++ src/calendar-utilities.js
@@ -1,3 +1,23 @@
+const currentDate = new Date();
+const currentYear = currentDate.getFullYear();
+const currentMonth = currentDate.getMonth();
+const isLeapYear =
+  currentYear % 4 === 0 && (currentYear % 100 !== 0 || currentYear % 400 === 0);
+const months = {
+  0: { name: "ocak", days: 31 },
+  1: { name: "şubat", days: isLeapYear ? 29 : 28 },
+  2: { name: "mart", days: 31 },
+  3: { name: "nisan", days: 30 },
+  4: { name: "mayıs", days: 31 },
+  5: { name: "haziran", days: 30 },
+  6: { name: "temmuz", days: 31 },
+  7: { name: "ağustos", days: 31 },
+  8: { name: "eylül", days: 30 },
+  9: { name: "ekim", days: 31 },
+  10: { name: "kasım", days: 30 },
+  11: { name: "aralık", days: 31 },
+};
+
 const range = (start, end, step = 1) => {
   return Array.from(
     { length: Math.floor((end - start) / step) + 1 },
@@ -19,4 +39,12 @@ const findSpecialDay = (specialDays, targetMonth, targetDay) => {
     .find((d) => d !== undefined);
 };
 
-export { range, capitalize, findSpecialDay };
+export {
+  currentDate,
+  currentYear,
+  currentMonth,
+  months,
+  range,
+  capitalize,
+  findSpecialDay,
+};
