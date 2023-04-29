c6ba638dd5443a59bdb187fa5c0a54bd7e0098ae
DrShahinstein <1taylanozturk@protonmail.com>
2023-04-29 18:20:54 +0300

    Include the new field in fields

--- server/api/serializers.py
+++ server/api/serializers.py
@@ -5,7 +5,8 @@ from core.models import Category, SpecialDay
 class SpecialDaySerializer(serializers.ModelSerializer):
     class Meta:
         model = SpecialDay
-        fields = ('title', 'description', 'month', 'day', 'highlight_color')
+        fields = ('title', 'description', 'month',
+                  'day', 'year', 'highlight_color')
 
 
 class CategorySerializer(serializers.ModelSerializer):
