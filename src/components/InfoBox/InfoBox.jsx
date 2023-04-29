77db70cd5a827b211a736e389d80b77c3e7f4a48
DrShahinstein <1taylanozturk@protonmail.com>
2023-04-29 17:53:27 +0300

    Implement the info-box animation using framer-motion pkg

--- src/components/InfoBox/InfoBox.jsx
+++ src/components/InfoBox/InfoBox.jsx
@@ -1,10 +1,20 @@
 import React from "react";
+import { motion } from "framer-motion";
 import { AiFillCloseCircle } from "react-icons/ai";
 import "./infobox.css";
 
 export default function InfoBox({ title, description, setInfoBox }) {
   return (
-    <div className="infobox-container">
+    <motion.div
+      className="infobox-container"
+      initial={{ scale: 0, rotate: 40 }}
+      animate={{ scale: 1, rotate: 0 }}
+      transition={{
+        type: "spring",
+        stiffness: 260,
+        damping: 20,
+      }}
+    >
       <div className="infobox">
         <div className="infobox-header">
           <h2>{title}</h2>
@@ -16,6 +26,6 @@ export default function InfoBox({ title, description, setInfoBox }) {
           <p>{description}</p>
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 }
